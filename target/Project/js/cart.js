4832f7a449d80ce9f0a700f9568d50fe974aa9fb
Dương Thanh Phụng <20130099@st.hcmuaf.edu.vn>
2022-12-17 14:30:18 +0700

    merge

--- target/Project/js/cart.js
+++ target/Project/js/cart.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-    var decoder = new TextDecoder('utf-8')
 
     $('.btn-remove').click(function () {
         let idProduct = $(this).val();
@@ -13,9 +12,14 @@ $(document).ready(function () {
             },
             success: function (response) {
                 let listResponse = response.replace(/\r/g, "").split(/\n/);
-                $('.total-real').text(listResponse[0] + ' VND');
-                $('.sale').text(listResponse[1] + ' VND');
-                $('.total').text(listResponse[2] + ' VND');
+                if (parseInt(listResponse[2].trim()) === 0) {
+                    removeEmlement();
+                } else {
+
+                    $('.total-real').text(listResponse[0] + ' VND');
+                    $('.sale').text(listResponse[1] + ' VND');
+                    $('.total').text(listResponse[2] + ' VND');
+                }
             },
             error: function (xhr) {
 
@@ -24,14 +28,8 @@ $(document).ready(function () {
     })
 
     $('.delete-all').click(function () {
+        removeEmlement();
 
-        $('.product').remove();
-        $('.bill').remove();
-        $('.list-product').removeClass('col-8');
-        $('.list-product').addClass('col-12');
-        $('.container-list-product').html('<li class="notification bd-bottom pb-4 pt-4 uppercase">\n' +
-            '            Giỏ hàng của bạn đang trống, quay lại mua hàng nhé!\n' +
-            '        </li>');
         $.ajax({
             url: "/cart/removeAllProduct",
             type: "get",
@@ -43,4 +41,16 @@ $(document).ready(function () {
             }
         });
     })
+
+    function removeEmlement() {
+        $('.product').remove();
+        $('.bill').remove();
+        $('.list-product').removeClass('col-8');
+        $('.list-product').addClass('col-12');
+        $('.container-list-product').html('<li class="notification bd-bottom pb-4 pt-4 uppercase">\n' +
+            '            Giỏ hàng của bạn đang trống, quay lại mua hàng nhé!\n' +
+            '        </li>');
+        $('.delete-all').remove();
+        $('.contain-btn').css('justify-content', 'center');
+    }
 })
